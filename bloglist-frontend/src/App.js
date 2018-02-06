0da3208c068f7f8e432f9114800572b597386abf
lex <lex@users.noreply.github.com>
2018-02-06 16:16:59 +0200

    5.1

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,28 +1,91 @@
-import React from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      blogs: []
-    }
+      blogs: [],
+      user: null,
+      username: "",
+      password: ""
+    };
   }
 
   componentDidMount() {
-    blogService.getAll().then(blogs =>
-      this.setState({ blogs })
-    )
-  } 
+    blogService.getAll().then(blogs => this.setState({ blogs }));
+  }
+
+  login = async event => {
+    event.preventDefault();
+
+    console.log(
+      `logging in with ${this.state.username}:${this.state.password}`
+    );
+
+    try {
+      const credentials = {
+        username: this.state.username,
+        password: this.state.password
+      };
+      const user = await loginService.login(credentials);
+      this.setState({ username: "", password: "", user });
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  handleLoginFieldChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  renderLogin() {
+    return (
+      <div>
+        <h2>Kirjaudu sovellukseen</h2>
+        <form onSubmit={this.login}>
+          <div>
+            username{" "}
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+
+          <div>
+            password{" "}
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+
+          <button type="submit">log in</button>
+        </form>
+      </div>
+    );
+  }
+
+  renderBlogs() {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>logged in as {this.state.user.name}</p>
+        {this.state.blogs.map(blog => <Blog key={blog._id} blog={blog} />)}
+      </div>
+    );
+  }
 
   render() {
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
+        {this.state.user === null ? this.renderLogin() : this.renderBlogs()}
       </div>
     );
   }
