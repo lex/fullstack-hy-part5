a075bf8d090b7900b59631ac9ecd49f0e71659c9
lex <lex@users.noreply.github.com>
2018-02-07 17:48:02 +0200

    5.6

--- bloglist-frontend/src/components/blog.js
+++ bloglist-frontend/src/components/blog.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showDetails: false };
+  }
+
+  toggleDetails = () => {
+    this.setState({ showDetails: !this.state.showDetails });
+  };
+
+  like = () => {};
+
+  render() {
+    const blogStyle = {
+      border: "1px solid black",
+      borderRadius: "5px",
+      margin: "5px",
+      padding: "5px"
+    };
+
+    return (
+      <div style={blogStyle}>
+        <p onClick={this.toggleDetails}>
+          {this.props.blog.title} {this.props.blog.author}
+        </p>
+
+        {this.state.showDetails && (
+          <div>
+            <div>
+              <a href={this.props.blog.url}>{this.props.blog.url}</a>
+            </div>
+            <div>
+              {this.props.blog.likes} likes{" "}
+              <button onClick={this.like}>like</button>
+            </div>
+            <div>
+              <p>
+                added by{" "}
+                {this.props.blog.user ? this.props.blog.user.name : "undefined"}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Blog;
