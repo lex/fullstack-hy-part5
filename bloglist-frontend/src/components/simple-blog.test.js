3a7df0efc78fa2f62a0521e9b5a39deb7ef4011f
lex <lex@users.noreply.github.com>
2018-02-08 13:47:30 +0200

    5.13

--- bloglist-frontend/src/components/simple-blog.test.js
+++ bloglist-frontend/src/components/simple-blog.test.js
@@ -18,4 +18,24 @@ describe("<SimpleBlog />", () => {
     expect(contentDiv.text()).toContain(blog.title);
     expect(likesDiv.text()).toContain(blog.likes);
   });
+
+  it("calls supplied function twice if like is clicked twice", () => {
+    const blog = {
+      author: "author",
+      title: "title",
+      likes: 4
+    };
+
+    const mockHandler = jest.fn();
+
+    const blogComponent = shallow(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    );
+    const button = blogComponent.find("button");
+
+    button.simulate("click");
+    button.simulate("click");
+
+    expect(mockHandler.mock.calls.length).toBe(2);
+  });
 });
